461128b1636b67b3bcbb402c4375752236cb75f5
RahulRajGiri15 <160770727+RahulRajGiri15@users.noreply.github.com>
2024-07-25 18:50:56 +0530

    This is my first commmit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-[#686D76] w-full h-screen '>
+      <div className='flex items-center justify-center w-40 rounded-full x-1/2 overflow-hidden bg-zinc-400'>
+            <img src="https://images.unsplash.com/photo-1519764622345-23439dd774f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGFuZHNvbWUlMjBib3l8ZW58MHx8MHx8fDA%3D" alt="" />
+      </div>
+      <div className='bg-blue-300 '>
+        <h1 className='text-center text-wrap '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam nam laboriosam obcaecati exercitationem minima, a pariatur beatae odit necessitatibus soluta consequuntur dolores. Exercitationem possimus, error laborum natus quisquam pariatur tenetur minima consequuntur architecto facere blanditiis eius laboriosam ipsum? Consequuntur a accusantium sit, voluptatibus ipsa blanditiis? Minima placeat tempora beatae nemo repellendus, quae vero! Quidem aliquam pariatur soluta hic qui sunt atque dicta neque, laborum unde minus totam possimus, quibusdam nihil officia voluptates. Voluptas temporibus nisi earum reiciendis ratione alias adipisci!   </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
